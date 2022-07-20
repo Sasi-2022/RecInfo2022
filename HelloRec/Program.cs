d97500a50ab67d2e42fb1271ec7f4f704676b9f6
Sasi-2022 <sasikalaumapathy0609@gmail.com>
2022-07-19 17:56:41 -0700

    Added new C# Console

---
+++ HelloRec/Program.cs
@@ -0,0 +1,27 @@
+﻿using system;
+
+
+public class OpenPanel : MonoBehaviour
+{
+    public GameObject gameObject;
+    bool active;
+
+    public void OpenandClose()
+    {
+        if (active == false)
+        {
+            gameObject.transform.gameObject.SetActive(true);
+            active = true;
+
+        }
+        else
+        {
+            gameObject.transform.gameObject.SetActive(false);
+            active = false;
+        }
+
+
+
+    }
+
+}
