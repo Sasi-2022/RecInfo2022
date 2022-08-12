05c9b290b753fc907d17a5b9d6683ede25686507
Sasi-2022 <sasikalaumapathy0609@gmail.com>
2022-08-11 17:12:30 -0700

    ChangeFolder

---
+++ 2Dgame/Assets/Scenes/SunnyLandGame2D/Scripts/Frog.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    [SerializeField] private float lefCap;
+    [SerializeField] private float rightCap;
+
+    [SerializeField] private float jumpLength = 10f;
+    [SerializeField] private float jumpHeight = 15f;
+    [SerializeField] private LayerMask ground;
+    private Collider2D coll;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+
+    private bool facingLeft = true;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Transition from Jump to fall
+        if (anim.GetBool("Jumping"))
+        {
+            if(rb.velocity.y < .1)
+            {
+                anim.SetBool("Falling", true);
+                anim.SetBool("Jumping", false);
+            }
+        }
+
+        //Transition from Fall to Idle
+        if (coll.IsTouchingLayers(ground) && anim.GetBool("Falling"))
+        {
+            anim.SetBool("Falling", false);
+        }
+    }
+
+    private void Move()
+    {
+        if (facingLeft)
+        {
+            if (transform.position.x > lefCap)
+            {
+                //Make sure sprite is facing right location, and if it is not, then face the right direction
+                if (transform.localScale.x != 1)
+                {
+                    transform.localScale = new Vector3(1, 1);
+                }
+
+                //Test to see if I am on the gorund, if so jump
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(-jumpLength, jumpHeight);
+                    anim.SetBool("Jumping", true);
+                }
+            }
+            else
+            {
+                facingLeft = false;
+            }
+        }
+
+        else
+        {
+            if (transform.position.x < rightCap)
+            {
+                //Make sure sprite is facing right location, and if it is not, then face the right direction
+                if (transform.localScale.x != -1)
+                {
+                    transform.localScale = new Vector3(-1, 1);
+                }
+
+                //Test to see if I am on the gorund, if so jump
+                if (coll.IsTouchingLayers(ground))
+                {
+                    rb.velocity = new Vector2(jumpLength, jumpHeight);
+                    anim.SetBool("Jumping", true);
+                }
+            }
+            else
+            {
+                facingLeft = false;
+            }
+        }
+    }
+}
